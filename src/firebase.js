69f15cee75a9300028822dfcb120b01f59c2fc6b
Bernie <bernw7214604@gmail.com>
2018-07-07 10:20:20 -0700

    Create and Read

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,7 @@
 import * as firebase from "firebase";
 
 const config = {
-    apiKey: "AIzaSyAfOULlhWiRyXo0OJ5Bpm7GwGcsli3cNyM",
-    authDomain: "manager-2d089.firebaseapp.com",
-    databaseURL: "https://manager-2d089.firebaseio.com",
-    projectId: "manager-2d089",
-    storageBucket: "manager-2d089.appspot.com",
-    messagingSenderId: "568067450040"
+
 };
 
 export default !firebase.apps.length ? firebase.initializeApp(config) : firebase.app();
